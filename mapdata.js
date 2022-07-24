2fecf3817dde10cd1c6b4b41a6614a305fc7ccf6
Saif Rahman <saif.rahman@udplatforms.com>
2022-07-24 23:29:18 +0600

    init

---
+++ mapdata.js
@@ -0,0 +1,146 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    KAZ3195: {
+      name: "Aqtöbe",
+      description: "Aqtöbe"
+    },
+    KAZ3196: {
+      name: "Qostanay",
+      description: "Qostanay"
+    },
+    KAZ3197: {
+      name: "Qyzylorda",
+      description: "Qyzylorda"
+    },
+    KAZ3198: {
+      name: "Atyrau",
+      description: "Atyrau"
+    },
+    KAZ3201: {
+      name: "West Kazakhstan",
+      description: "West Kazakhstan"
+    },
+    KAZ3202: {
+      name: "Aqmola",
+      description: "Aqmola"
+    },
+    KAZ3203: {
+      name: "Qaraghandy",
+      description: "Qaraghandy"
+    },
+    KAZ3204: {
+      name: "North Kazakhstan",
+      description: "North Kazakhstan"
+    },
+    KAZ3205: {
+      name: "Pavlodar",
+      description: "Pavlodar"
+    },
+    KAZ3206: {
+      name: "East Kazakhstan",
+      description: "East Kazakhstan"
+    },
+    KAZ3207: {
+      name: "Almaty",
+      description: "Almaty"
+    },
+    KAZ3236: {
+      name: "Mangghystau",
+      description: "Mangghystau"
+    },
+    KAZ3251: {
+      name: "South Kazakhstan",
+      description: "South Kazakhstan"
+    },
+    KAZ3252: {
+      name: "Zhambyl",
+      description: "Zhambyl"
+    },
+    KAZ4829: {
+      name: "Almaty City",
+      description: "Almaty City"
+    },
+    KAZ4830: {
+      name: "Astana",
+      description: "Astana"
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
